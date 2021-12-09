e88c487bf47f663a07af477480c609dc9c9720ed
v.bandurka <shadwork@mail.ru>
2021-12-09 15:53:02 +0200

    Add library

--- app/src/main/java/com/sample/arch/forbidden/DialogActivity.java
+++ app/src/main/java/com/sample/arch/forbidden/DialogActivity.java
@@ -2,12 +2,16 @@ package com.sample.arch.forbidden;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.OnItemClickListener;
 
 public class DialogActivity extends AppCompatActivity {
 
@@ -17,7 +21,8 @@ public class DialogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dialog);
 
         findViewById(R.id.buttonDialogTitled).setOnClickListener((view) -> showTitledDialog());
-
+        findViewById(R.id.buttonDialogPlus).setOnClickListener((view) -> showPlusDialog());
+        findViewById(R.id.buttonDialogMaterial).setOnClickListener((view) -> showMaterialDialog());
     }
 
     void showTitledDialog() {
@@ -33,6 +38,25 @@ public class DialogActivity extends AppCompatActivity {
             }
         });
         builder.create().show();
+    }
+
+    void showPlusDialog() {
+        BaseAdapter dialogAdapter = ArrayAdapter.createFromResource(this, R.array.dialog_plus_items,
+                android.R.layout.simple_spinner_item);
+        DialogPlus dialog = DialogPlus.newDialog(this)
+                .setAdapter(dialogAdapter)
+                .setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(DialogPlus dialog, Object item, View view, int position) {
+                    }
+                })
+                .setExpanded(true)
+                .create();
+        dialog.show();
+    }
+
+    void showMaterialDialog() {
+
     }
 
 }
